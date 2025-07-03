5a744c96494be6c977b45e2d157d0ccd9ec08999
drulacosmin <drulacosmin@gmail.com>
2025-07-03 05:51:57 +0300

    Update gketap.rb

--- Formula/gketap.rb
+++ Formula/gketap.rb
@@ -10,8 +10,8 @@ class Gketap < Formula
   depends_on "python@3.11"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/49/d6/00f0f5e546ef9dfb1792c7b67c80a271b1773e0a25f338beeb8dc1b23fbd/click-8.1.7.tar.gz"
-    sha256 "7c8c58e4bfc476e8428de1be72d3572ce91db8df0f9fd5c1d1f4197866b0150b"
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
   end
 
   resource "pyyaml" do
